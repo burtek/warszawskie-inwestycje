21c8e16e008d4cfffac15206444ff67d47f5c9d0
Bartosz Dotryw <burtekdotryw@gmail.com>
2021-09-13 08:23:40 +0200

    Remove eslint rule for data json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,9 @@
-const rulesDirPlugin = require('eslint-plugin-rulesdir');
-
-rulesDirPlugin.RULES_DIR = 'eslint-rules';
-
 /**
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
     extends: 'next/core-web-vitals',
-    plugins: ['rulesdir'],
     rules: {
         'react/jsx-no-undef': 'off'
-    },
-    overrides: [
-        {
-            files: ['src/data/index.ts'],
-            rules: {
-                'rulesdir/id-checker': 'error'
-            }
-        }
-    ]
+    }
 };
